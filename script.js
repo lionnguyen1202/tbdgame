942bdc262e4d6b82d9da1878ab402acea1bd8513
lionnguyen1202 <131339214+lionnguyen1202@users.noreply.github.com>
2023-04-20 15:24:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,42 +32,80 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 15, src: 'https://i.postimg.cc/YhY7wFRH/pokemon10.png' },
     ];
 
-    let doubledCards = cards.concat(cards);
-    doubledCards.sort(() => 0.5 - Math.random());
+  let shuffledCards = shuffleArray(cards);
+  let firstCard = null;
+  let secondCard = null;
+  let canFlip = true;
 
-    const memoryGame = document.querySelector('.memory-game');
+  function createCard(card) {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.dataset.id = card.id;
 
-    doubledCards.forEach(card => {
-        const memoryCard = document.createElement('div');
-        memoryCard.classList.add('memory-card');
-        memoryCard.dataset.id = card.id;
+    const cardFront = document.createElement('img');
+    cardFront.src = card.src;
+    cardFront.classList.add('front');
 
-        const frontImage = document.createElement('img');
-        frontImage.src = card.src;
-        frontImage.style.transform = 'rotateY(180deg)';
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('back');
 
-        memoryCard.appendChild(frontImage);
-        memoryGame.appendChild(memoryCard);
+    cardElement.appendChild(cardFront);
+    cardElement.appendChild(cardBack);
+
+    cardElement.addEventListener('click', () => {
+      if (!canFlip) return;
+      if (cardElement.classList.contains('flipped')) return;
+
+      cardElement.classList.add('flipped');
+
+      if (!firstCard) {
+        firstCard = cardElement;
+      } else {
+        secondCard = cardElement;
+
+        if (firstCard.dataset.id === secondCard.dataset.id) {
+          setTimeout(() => {
+            firstCard.remove();
+            secondCard.remove();
+
+            firstCard = null;
+            secondCard = null;
+          }, 1000);
+        } else {
+          canFlip = false;
+          setTimeout(() => {
+            firstCard.classList.remove('flipped');
+            secondCard.classList.remove('flipped');
+
+            firstCard = null;
+            secondCard = null;
+            canFlip = true;
+          }, 1000);
+        }
+      }
     });
 
-    let flippedCards = [];
-    memoryGame.addEventListener('click', (event) => {
-        const memoryCard = event.target.closest('.memory-card');
+    return cardElement;
+  }
 
-        if (!memoryCard || memoryCard.classList.contains('flip')) return;
+  function shuffleArray(array) {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  }
 
-        memoryCard.classList.add('flip');
-        flippedCards.push(memoryCard);
+  function initializeGame() {
+    const gameBoard = document.getElementById('game-board');
+    gameBoard.innerHTML = '';
 
-        if (flippedCards.length === 2) {
-            setTimeout(() => {
-                if (flippedCards[0].dataset.id === flippedCards[1].dataset.id) {
-flippedCards.forEach(card => card.classList.add('matched'));
-} else {
-flippedCards.forEach(card => card.classList.remove('flip'));
-}
-flippedCards = [];
-}, 1000);
-}
-});
+    shuffledCards.forEach((card) => {
+      const cardElement = createCard(card);
+      gameBoard.appendChild(cardElement);
+    });
+  }
+
+  initializeGame();
 });
